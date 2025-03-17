1ca3710ed0c97445a4dc513d65767517a914f971
DukeAkinOluwa <akindukoakinoluwa@gmail.com>
2025-03-17 21:16:44 +0100

    Update

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker } from "react-day-picker";
+import { DayPicker, type DayPickerProps } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+export type CalendarProps = DayPickerProps;
 
 function Calendar({
   className,
@@ -27,8 +27,8 @@ function Calendar({
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
+        nav_button_previous: "absolute left-1 [&>svg]:w-4 [&>svg]:h-4",
+        nav_button_next: "absolute right-1 [&>svg]:w-4 [&>svg]:h-4",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
@@ -51,16 +51,11 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        // IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        // IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
-      }}
       {...props}
     />
   );
 }
+
 Calendar.displayName = "Calendar";
 
 export { Calendar };
